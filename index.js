8b270eb5be29f271d42331c42cc7d8eaaacaebbb
Milad Nekofar <milad@nekofar.com>
2019-06-20 09:00:55 +0430

    Add functionality required for get fontiran login and font info

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 'use strict';
 
+var fonts = require('./fonts.json');
+
 var through = require('through2');
+var dotenv = require('dotenv');
 
 var PluginError = require('gulp-error');
 
 var PLUGIN_NAME = 'gulp-fontiran';
 
-module.exports = function () {
+module.exports = function (slug) {
+
+    // Make sure font slug passed
+    if (!slug || slug === "") {
+        new PluginError(PLUGIN_NAME, 'font slug missing.');
+    }
+
     return through.obj(function (file, enc, callback) {
         var self = this;
 
@@ -14,6 +23,28 @@ module.exports = function () {
             self.push(file);
             return callback();
         }
+
+        // Load configs from dotenv file
+        var config = dotenv.config({path: file.path});
+        if (config.error) {
+            new PluginError(PLUGIN_NAME, 'dotenv config file missing.');
+        }
+
+        // Get font info using slug
+        if (typeof fonts[slug] === 'undefined') {
+            new PluginError(PLUGIN_NAME, 'font not found.');
+        }
+
+        // Options
+        var opts = {
+            fontInfo: fonts[slug],
+            username: process.env.FI_USER,
+            password: process.env.FI_PASS,
+            hostName: 'fontiran.com',
+            hostPath: '/wp-admin/admin-ajax.php'
+        };
+
+
 
     });
 };
