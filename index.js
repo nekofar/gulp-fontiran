40d51e6526275ef22eeae0d20e27e2fe7d315670
Milad Nekofar <milad@nekofar.com>
2019-06-20 09:05:01 +0430

    Add functionality required for login and fetch font package url

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var fonts = require('./fonts.json');
 
 var through = require('through2');
 var dotenv = require('dotenv');
+var async = require('async');
+var request = require('request');
 
 var PluginError = require('gulp-error');
 
@@ -44,7 +46,55 @@ module.exports = function (slug) {
             hostPath: '/wp-admin/admin-ajax.php'
         };
 
+        /**
+         * Login to site and pass the cookie
+         *
+         * @param callback
+         */
+        function loginToSite(callback) {
+            var jar = request.jar();
+            var form = {
+                log: opts.username,
+                pwd: opts.password,
+                type: 'cartFromLogin',
+                action: 'my_action'
+            };
+            request.post({
+                url: 'https://' + opts.hostName + opts.hostPath,
+                form: form,
+                jar: jar
+            }, function (error, response, body) {
 
+                callback(null, jar);
+            });
+        }
+
+        /**
+         * Fetch the temporary download link of the font
+         *
+         * @param jar
+         * @param callback
+         */
+        function fetchFontLink(jar, callback) {
+            var form = {
+                type: 'downloadRequest',
+                action: 'my_action',
+                package_id: opts.fontInfo.id
+            };
+            request.post({
+                url: 'https://' + opts.hostName + opts.hostPath,
+                form: form,
+                jar: jar,
+                json: true
+            }, function (error, response, body) {
+                if (body && body.message) {
+                    var url = body.message.match(/(\bhttps?:\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i);
+                    callback(null, url[0]);
+                }
+            });
+        }
+
+        async.waterfall([loginToSite, fetchFontLink], callback);
 
     });
 };
